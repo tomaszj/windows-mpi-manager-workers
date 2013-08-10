c710f65edd378b9099b6430e4fd807713e603ea4
Tomasz Janeczko <janecztom@gmail.com>
2013-08-10 13:50:29 +0200

    Added better result receiving code - now it waits for any message, not caring about the order

--- MPI_ManagerWorkerSample/JobManager.cpp
+++ MPI_ManagerWorkerSample/JobManager.cpp
@@ -19,25 +19,24 @@ void JobManager::manage_processes(void)
 
 	// Find the amount of processes available
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
     cout << "Starting managing workers in pool of " << world_size << endl;
 
+    // Setup and send the work for each node
 	for (current_rank = 1; current_rank < world_size; current_rank++)
 	{
         int work_number = 333;
-
-        cout << "[Manager] Sent " << work_number << " to " << current_rank << endl;
-
         MPI_Send(&work_number, 1, MPI_INT, current_rank, 100, MPI_COMM_WORLD);        
+        cout << "[Manager] Sent " << work_number << " to " << current_rank << endl;
 	}
 
+    // Wait for the result for each node
     for (current_rank = 1; current_rank < world_size; current_rank++)
     {
         int received_work_number;
         MPI_Status status;
-        MPI_Recv(&received_work_number, 1, MPI_INT, current_rank, 100, MPI_COMM_WORLD, &status);
+        MPI_Recv(&received_work_number, 1, MPI_INT, MPI_ANY_SOURCE, 100, MPI_COMM_WORLD, &status);
 
-        cout << "[Manager] Received " << received_work_number << " from " << current_rank << endl;
+        cout << "[Manager] Received " << received_work_number << " from #" << status.MPI_SOURCE << endl;
     }
 
     system("pause");
--- MPI_ManagerWorkerSample/Worker.cpp
+++ MPI_ManagerWorkerSample/Worker.cpp
@@ -20,14 +20,15 @@ void Worker::start_work(void)
 
     // Determine current rank
     MPI_Comm_rank(MPI_COMM_WORLD, &worker_rank);
-
     cout << "[Worker #" << worker_rank << "] starting work" << endl;
-
+    
+    // Receive work to be processed
     MPI_Recv(&work_number, 1, MPI_INT, 0, 100, MPI_COMM_WORLD, &status);
-
     cout << "[Worker #" << worker_rank << "] received: " << work_number << endl;
 
+    // Perform the work
     int result_work_number = work_number * 2;
 
+    // Send back results
     MPI_Send(&result_work_number, 1, MPI_INT, 0, 100, MPI_COMM_WORLD);
 }
