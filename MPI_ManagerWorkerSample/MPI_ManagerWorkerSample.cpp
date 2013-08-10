705f6be1df12877f969b311204ceb8c6775a3dea
Tomasz Janeczko <janecztom@gmail.com>
2013-08-10 10:15:02 +0200

    Added initial project

---
+++ MPI_ManagerWorkerSample/MPI_ManagerWorkerSample.cpp
@@ -0,0 +1,14 @@
+// MPI_ManagerWorkerSample.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	system("pause");
+
+	return 0;
+}
+
